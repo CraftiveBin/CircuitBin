3a582c3566946d17d504e49b42393bf3ced5d190
sibinks <75927720+CraftiveBin@users.noreply.github.com>
2021-04-07 15:00:05 +0530

    Create schedule.ino

---
+++ schedule.ino
@@ -0,0 +1,70 @@
+
+bool data;
+long onTime, offTime;
+byte SumON, SumOFF; // number of programms on and number of programmes off
+void process()      // ! PLEASE DO NOT DISTURB THE PROGRAM SEQUENCE OF PROCESS LOOP !
+{
+  static byte PrevSeconds = 1; //let it run at initialisation
+  if (Second != PrevSeconds)
+  {
+    for (int i = 0; i < (progMax * 7); i += 7)
+    {
+      DateTime on(Year, Month, Day, readEEPROM(i), readEEPROM(i + 1), readEEPROM(i + 2));
+      DateTime off(Year, Month, Day, readEEPROM(i + 3), readEEPROM(i + 4), readEEPROM(i + 5));
+
+      onTime = on.unixtime();
+      offTime = off.unixtime();
+
+      if (onTime > offTime)
+      {
+        DateTime off(Year, Month, (Day + 1), readEEPROM(i + 3), readEEPROM(i + 4), readEEPROM(i + 5));
+        offTime = off.unixtime();
+      }
+      bool temp = pgm_read_byte(&weekPing[(readEEPROM(i + 6))][Week]); // get programmed week from PROGMEM
+
+      if ((onTime <= unixNow) && (unixNow < offTime) && (temp == 1))
+      {
+        SumON++;
+      }
+      else
+      {
+        SumOFF--;
+      }
+    }
+
+    if (SumON > 0)
+    {
+      processOut = HIGH;
+    }
+    else
+    {
+      processOut = LOW;
+    }
+
+    SumOFF = 0; // always put this at last
+    SumON = 0;  // always put this at last*/
+
+    PrevSeconds = Second;
+  }
+
+  if (processOut == 1 && event5 == 0)
+  {
+    digitalWrite(Station01, HIGH);
+    Station = HIGH;
+  }
+  else if (event5 == 1)
+  {
+    digitalWrite(Station01, HIGH);
+    Station = HIGH;
+  }
+  else if (event5 == 2)
+  {
+    digitalWrite(Station01, LOW);
+    Station = LOW;
+  }
+  else
+  {
+    digitalWrite(Station01, LOW);
+    Station = LOW;
+  }
+}
